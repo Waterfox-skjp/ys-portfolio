287b69dee98f1bc1807b1a83c7763a35f3220849
水狐 <yuki.skline@gmail.com>
2023-08-12 17:19:15 +0900

    BASIC認証 追加

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization');
+  const url = req.nextUrl;
+
+  if (process.env.NODE_ENV === 'production') {
+    if (basicAuth) {
+      const authValue = basicAuth.split(' ')[1];
+      const [user, pwd] = atob(authValue).split(':');
+
+      if (user === 'Recruiter' && pwd === process.env.RECRUITER_PW) {
+        return NextResponse.next();
+      } else if (user === 'Owner' && pwd === process.env.OWNER_PW) {
+
+      }
+    }
+    url.pathname = '/api/auth';
+
+    return NextResponse.rewrite(url);
+  }
+}
