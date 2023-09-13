8570bb8637ff98f880342e0fd8eced76b915d5cd
水狐 <yuki.skline@gmail.com>
2023-09-14 00:01:07 +0900

    使用フレームワーク 並び順をID順に修正

--- components/useFramework.tsx
+++ components/useFramework.tsx
@@ -5,18 +5,36 @@ type Props = {
   useFramework: string[]
 }
 
+type FrameworkItem = {
+  id: number
+  name: string
+  src: string
+}
+
 export default function useFramework( { useFramework } : Props ) {
   const frameworkItems = frameworkData.item
+
+  // ID順に並び替える
+  const useFrameworkSort: FrameworkItem[] = []
+  useFramework.map((value)=>{
+    const frameworkItem = frameworkItems.find((item) => item.name === value)
+    if (frameworkItem) {
+      useFrameworkSort.push(frameworkItem)
+    }
+  })
+  useFrameworkSort.sort((a, b) => a.id - b.id)
+
   return (
-    <ul className="p-article__contents-framework">
-      {useFramework.map((value) => {
-        const frameworkItem = frameworkItems.find((item) => item.name === value)
-        return (
-          <li key={frameworkItem?.id}>
-            <Image src={frameworkItem?.src ? frameworkItem?.src : ''} alt={frameworkItem?.name ? frameworkItem?.name : ''} width="50" height="50" />
-          </li>
-        )
-      })}
-    </ul>
+    <>
+      <ul className="p-article__contents-framework">
+        {useFrameworkSort.map((value) => {
+          return (
+            <li key={value.id}>
+              <Image src={value.src} alt={value.name} width="50" height="50" />
+            </li>
+          )
+        })}
+      </ul>
+    </>
   )
 }
